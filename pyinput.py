1c8430387ee63a1ae74f94494e6e6386fbf00b1d
Morgan Meliment <morganmeliment@gmail.com>
2016-11-01 14:30:11 -0400

    Updated from Brython Server: 11/1/2016 2:30:11 PM

--- pyinput.py
+++ pyinput.py
@@ -8,6 +8,7 @@ try:
 	
 	if not jq(".inputScreen").length:
 		jq("body").append('<div class = "inputScreen" style = "position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2); z-index: 50;"><div style = "position: fixed; left: calc(50% - 150px); top: calc(50% - 75px); background-color: white;"><p id = "toPrompt"></p><input type = "text" id = "toInput"><input type = "submit" id = "toSubmit"></div></div>')
+		jq(".inputScreen").hide()
 
 	def input_callback():
 	    input_value = jq('#toInput').val()
@@ -37,3 +38,11 @@ except:
 		x = underInput(text+" ")
 		callback(text, x)
 		
+#def first(text, result):
+#    print("Hi " + result)
+#    input("What's your favorite color?", second)
+
+
+#def second(text, result):
+#    print("Nice! I like " + result + " too.")
+
